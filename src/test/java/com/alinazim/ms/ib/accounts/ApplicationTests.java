e74100f727cc66a4da860dac1d8dc0f45c1d2411
alihuseynaliyev <alihuseyneliyev12@gmail.com>
2023-08-09 13:33:24 +0400

    init commit

---
+++ src/test/java/com/alinazim/ms/ib/accounts/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alinazim.ms.ib.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
